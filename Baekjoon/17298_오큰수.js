baa0582c7fcc0d3f3ef4609398796d7ed0de7dd5
aiden <aiden@fanding.kr>
2023-09-19 19:44:50 +0900

    add: 오큰수

---
+++ Baekjoon/17298_오큰수.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, arr] = input;
