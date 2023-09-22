d4f1f8f8f476afcf752fdbb91f0fc7084ee678f3
aiden <aiden@fanding.kr>
2023-09-22 16:32:56 +0900

    fix: 오등큰수 정답 제출

--- Baekjoon/17299_오등큰수.js
+++ Baekjoon/17299_오등큰수.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const input = fs
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
+  )
   .toString()
   .trim()
   .split("\n");
@@ -8,19 +10,21 @@ const input = fs
 const [_, inputs] = input;
 const numbers = inputs.split(" ").map(Number);
 const N = numbers.length;
-const counts = Array(N + 1).fill(0);
+const counts = {};
 const stack = [];
-const answer = [];
+const answer = Array(N).fill(-1);
 
+numbers.forEach((number) => {
+  counts[number] = (counts[number] || 0) + 1;
+});
 for (let i = 0; i < N; i++) {
-  counts[numbers[i]] += 1;
   while (
     stack.length &&
-    counts[numbers[i]] < counts[numbers[stack[stack.length - 1]]]
+    counts[numbers[i]] > counts[numbers[stack[stack.length - 1]]]
   ) {
-    answer.push(numbers[stack.pop()]);
+    answer[stack.pop()] = numbers[i];
   }
   stack.push(i);
 }
 
-console.log(answer, stack);
+console.log(answer.join(" "));
